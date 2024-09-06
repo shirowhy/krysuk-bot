657dbc7e574d16b3cd4a1e6bf5e0cbe2ec95661d
shirowhy <yanawhitty@gmail.com>
2024-09-06 20:29:30 +0200

    Minor fixes

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,14 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyADBr0mkExTa-VKv-niwVqPQiaiwgKfy8A",
-  authDomain: "krysuk-bot.firebaseapp.com",
-  projectId: "krysuk-bot",
-  storageBucket: "krysuk-bot.appspot.com",
-  messagingSenderId: "801896618946",
-  appId: "1:801896618946:web:fd62d695acb2482551945b",
-  measurementId: "G-DEE2JYJQ14"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
