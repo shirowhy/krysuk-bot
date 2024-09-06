23ede2ace834f54ecb6fbc3c6b7ca1f80a0ff0c1
shirowhy <yanawhitty@gmail.com>
2024-09-06 23:16:26 +0200

    Fix farebase integration

--- src/helpers/messageCollector.ts
+++ src/helpers/messageCollector.ts
@@ -1,6 +1,5 @@
 import { MessageContext } from 'vk-io';
 import { db } from '../firebase';
-import { collection, addDoc } from 'firebase/firestore';
 
 export const collectMessage = async (context: MessageContext) => {
   const message = {
@@ -10,7 +9,7 @@ export const collectMessage = async (context: MessageContext) => {
   };
 
   try {
-    const docRef = await addDoc(collection(db, "messages"), message);
+    const docRef = await db.collection("messages").add(message);
     console.log("Message saved with ID: ", docRef.id);
   } catch (e) {
     console.error("Error saving message: ", e);
