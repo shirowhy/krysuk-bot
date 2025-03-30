8fd2682ce90104d40d4a66067e7255c1832e2519
shirowhy <yanawhitty@gmail.com>
2025-03-30 19:50:07 +0200

    Change firebase creds format

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,12 @@
 import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
-const serviceAccount = {
-  "type": process.env.FIREBASE_TYPE,
-  "project_id": process.env.FIREBASE_PROJECT_ID,
-  "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
-  "private_key": process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-  "client_email": process.env.FIREBASE_CLIENT_EMAIL,
-  "client_id": process.env.FIREBASE_CLIENT_ID,
-  "auth_uri": process.env.FIREBASE_AUTH_URI,
-  "token_uri": process.env.FIREBASE_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL,
-  "universe_domain": process.env.UNIVERSE_DOMAIN
-};
+const raw = process.env.FIREBASE_CREDENTIALS;
+if (!raw) {
+  throw new Error('FIREBASE_CREDENTIALS is not set');
+}
+
+const serviceAccount = JSON.parse(raw);
 
 const app = initializeApp({
   credential: cert(serviceAccount as any)
